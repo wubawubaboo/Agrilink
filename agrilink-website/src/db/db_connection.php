a26d220170a594da0aa9365a3f7a56a6e45cb2a2
wubawubaboo <ludwig.jose26@gmail.com>
2025-05-31 22:32:04 +0800

    Update db_connection.php

--- agrilink-website/src/db/db_connection.php
+++ agrilink-website/src/db/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "imsherlocked26@";
+$password = "";
 $database = "agrilink";
 
 $connection = new mysqli($servername, $username, $password, $database);
